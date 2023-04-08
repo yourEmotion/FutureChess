f6605e64052db8987274cedbaa789ed745a1af98
Ermoshin Mikhail <115833639+yourEmotion@users.noreply.github.com>
2023-04-09 00:07:36 +0300

    Create ChessMain.py

---
+++ Chess/ChessMain.py
@@ -0,0 +1,101 @@
+"""
+Это основной драйвер игры, он будет преобразовывать входные данные и показывать текущее состояние объекта
+"""
+
+import pygame as p
+import ChessEngine
+
+WIDTH = HEIGHT = 512
+DIMENSION = 8
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15
+IMAGES = {}
+
+
+def loadImages():
+    pieces = ['wp', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bp', 'bR', 'bN', 'bB', 'bQ', 'bK']
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+
+
+def Game():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = ChessEngine.GameState()
+    valid_moves = gs.getValidMoves()
+    move_is_made = False
+    loadImages()
+    running = True
+    sqSelected = ()
+    playerClicks = []
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+            #если нажата кнопка мыши
+            elif e.type == p.MOUSEBUTTONDOWN:
+                location = p.mouse.get_pos()
+                col = location[0] // SQ_SIZE
+                row = location[1] // SQ_SIZE
+                if sqSelected == (row, col) or (sqSelected == () and gs.board[row][col] == "--"):
+                    sqSelected = ()
+                    playerClicks = []
+                else:
+                    sqSelected = (row, col)
+                    playerClicks.append(sqSelected)
+                if len(playerClicks) == 2:
+                    move = ChessEngine.Move(playerClicks[0], playerClicks[1], gs.board)
+                    if move in valid_moves:
+                        move_is_made = True
+                        print(move.getChessNotation())
+                        gs.makeMove(move)
+                        sqSelected = ()
+                        playerClicks = []
+                    elif gs.board[row][col] == "--":
+                        sqSelected = ()
+                        playerClicks = []
+                    else:
+                        playerClicks = [sqSelected]
+
+
+            #нажата клавиша на клавиатуре
+            elif e.type == p.KEYDOWN:
+                if e.key == p.K_LCTRL:
+                    gs.undoMove()
+                    move_is_made = True
+        if move_is_made:
+            valid_moves = gs.getValidMoves()
+            move_is_made = False
+        drawGameState(screen, gs, sqSelected)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+    p.quit()
+
+
+def drawGameState(screen, gs, sqSelected):
+    drawBoard(screen)
+    drawPieces(screen, gs.board)
+    if (len(sqSelected) != 0):
+        drawSelectLine(screen, gs.board, sqSelected[0], sqSelected[1])
+
+
+def drawBoard(screen):
+    colors = [p.Color("white"), p.Color("grey")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r + c) % 2)]
+            p.draw.rect(screen, color, p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != "--":
+                screen.blit(IMAGES[piece], p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+def drawSelectLine(screen, board, row, col):
+    p.draw.rect(screen, p.Color("green"), p.Rect(col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE), 3)
+
