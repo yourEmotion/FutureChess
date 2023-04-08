011cf8805bd6e55691061ed5536d5d7396a3acf9
Ilia Gromakov <gromakov.ia@phystech.edu>
2023-04-08 09:48:53 +0300

    Исправил баг с ходом пустой клетки. Добавил выделение фигуры при её выборе.

--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -36,7 +36,7 @@ def main():
                 location = p.mouse.get_pos()
                 col = location[0] // SQ_SIZE
                 row = location[1] // SQ_SIZE
-                if sqSelected == (row, col):
+                if sqSelected == (row, col) or (sqSelected == () and gs.board[row][col] == "--"):
                     sqSelected = ()
                     playerClicks = []
                 else:
@@ -49,14 +49,16 @@ def main():
                     sqSelected = ()
                     playerClicks = []
 
-        drawGameState(screen, gs)
+        drawGameState(screen, gs, sqSelected)
         clock.tick(MAX_FPS)
         p.display.flip()
 
 
-def drawGameState(screen, gs):
+def drawGameState(screen, gs, sqSelected):
     drawBoard(screen)
     drawPieces(screen, gs.board)
+    if (len(sqSelected) != 0):
+        drawSelectLine(screen, gs.board, sqSelected[0], sqSelected[1])
 
 
 def drawBoard(screen):
@@ -74,6 +76,8 @@ def drawPieces(screen, board):
             if piece != "--":
                 screen.blit(IMAGES[piece], p.Rect(c * SQ_SIZE, r * SQ_SIZE, SQ_SIZE, SQ_SIZE))
 
+def drawSelectLine(screen, board, row, col):
+    p.draw.rect(screen, p.Color("green"), p.Rect(col * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE), 3)
 
 if __name__ == "__main__":
     main()
